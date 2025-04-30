b0bd1e8c97234b51b20bef064a1fd4f48fd79809
James Garbutt <43081j@users.noreply.github.com>
2025-04-30 10:27:26 +0100

    perf(no-thenable): switch to raw rule
    Part of #16.
    
    This one reduces the timing from ~85ms to ~30ms, easy win.

--- rules/no-thenable.js
+++ rules/no-thenable.js
@@ -16,134 +16,100 @@ const isStringThen = (node, context) =>
 const isPropertyThen = (node, context) =>
 	getPropertyName(node, context.sourceCode.getScope(node)) === 'then';
 
-const cases = [
-	// `{then() {}}`,
-	// `{get then() {}}`,
-	// `{[computedKey]() {}}`,
-	// `{get [computedKey]() {}}`,
-	{
-		selector: 'ObjectExpression',
-		*getNodes(node, context) {
+/** @param {import('eslint').Rule.RuleContext} context */
+const create = (context) => {
+	return {
+		// `{then() {}}`,
+		// `{get then() {}}`,
+		// `{[computedKey]() {}}`,
+		// `{get [computedKey]() {}}`,
+		ObjectExpression(node) {
 			for (const property of node.properties) {
 				if (property.type === 'Property' && isPropertyThen(property, context)) {
-					yield property.key;
+					context.report({node: property.key, messageId: MESSAGE_ID_OBJECT});
 				}
 			}
 		},
-		messageId: MESSAGE_ID_OBJECT,
-	},
-	// `class Foo {then}`,
-	// `class Foo {static then}`,
-	// `class Foo {get then() {}}`,
-	// `class Foo {static get then() {}}`,
-	{
-		selectors: ['PropertyDefinition', 'MethodDefinition'],
-		*getNodes(node, context) {
-			if (getPropertyName(node, context.sourceCode.getScope(node)) === 'then') {
-				yield node.key;
+
+		// `class Foo {then}`,
+		// `class Foo {static then}`,
+		// `class Foo {get then() {}}`,
+		// `class Foo {static get then() {}}`,
+		'PropertyDefinition, MethodDefinition'(node) {
+			if (isPropertyThen(node, context)) {
+				context.report({node: node.key, messageId: MESSAGE_ID_CLASS});
 			}
 		},
-		messageId: MESSAGE_ID_CLASS,
-	},
-	// `foo.then = …`
-	// `foo[computedKey] = …`
-	{
-		selector: 'MemberExpression',
-		*getNodes(node, context) {
-			if (
-				!(
-					node.parent.type === 'AssignmentExpression' &&
-					node.parent.left === node
-				)
-			) {
-				return;
-			}
 
-			if (getPropertyName(node, context.sourceCode.getScope(node)) === 'then') {
-				yield node.property;
+		// `foo.then = …`
+		// `foo[computedKey] = …`
+		MemberExpression(node) {
+			if (
+				node.parent.type === 'AssignmentExpression' &&
+				node.parent.left === node &&
+				isPropertyThen(node, context)
+			) {
+				context.report({node: node.property, messageId: MESSAGE_ID_OBJECT});
 			}
 		},
-		messageId: MESSAGE_ID_OBJECT,
-	},
-	// `Object.defineProperty(foo, 'then', …)`
-	// `Reflect.defineProperty(foo, 'then', …)`
-	{
-		selector: 'CallExpression',
-		*getNodes(node, context) {
+
+		// `Object.defineProperty(foo, 'then', …)`
+		// `Reflect.defineProperty(foo, 'then', …)`
+		CallExpression(node) {
 			if (
-				!(
-					isMethodCall(node, {
-						objects: ['Object', 'Reflect'],
-						method: 'defineProperty',
-						minimumArguments: 3,
-						optionalCall: false,
-						optionalMember: false,
-					}) && node.arguments[0].type !== 'SpreadElement'
-				)
+				isMethodCall(node, {
+					objects: ['Object', 'Reflect'],
+					method: 'defineProperty',
+					minimumArguments: 3,
+					optionalCall: false,
+					optionalMember: false,
+				}) &&
+				node.arguments[0].type !== 'SpreadElement'
 			) {
-				return;
+				const [, secondArgument] = node.arguments;
+				if (isStringThen(secondArgument, context)) {
+					context.report({node: secondArgument, messageId: MESSAGE_ID_OBJECT});
+				}
 			}
 
-			const [, secondArgument] = node.arguments;
-			if (isStringThen(secondArgument, context)) {
-				yield secondArgument;
-			}
-		},
-		messageId: MESSAGE_ID_OBJECT,
-	},
-	// `Object.fromEntries([['then', …]])`
-	{
-		selector: 'CallExpression',
-		*getNodes(node, context) {
+			// `Object.fromEntries([['then', …]])`
 			if (
-				!(
-					isMethodCall(node, {
-						object: 'Object',
-						method: 'fromEntries',
-						argumentsLength: 1,
-						optionalCall: false,
-						optionalMember: false,
-					}) && node.arguments[0].type === 'ArrayExpression'
-				)
+				isMethodCall(node, {
+					object: 'Object',
+					method: 'fromEntries',
+					argumentsLength: 1,
+					optionalCall: false,
+					optionalMember: false,
+				}) &&
+				node.arguments[0].type === 'ArrayExpression'
 			) {
-				return;
-			}
-
-			for (const pairs of node.arguments[0].elements) {
-				if (
-					pairs?.type === 'ArrayExpression' &&
-					pairs.elements[0] &&
-					pairs.elements[0].type !== 'SpreadElement'
-				) {
-					const [key] = pairs.elements;
-
-					if (isStringThen(key, context)) {
-						yield key;
+				for (const pairs of node.arguments[0].elements) {
+					if (
+						pairs?.type === 'ArrayExpression' &&
+						pairs.elements[0] &&
+						pairs.elements[0].type !== 'SpreadElement'
+					) {
+						const [key] = pairs.elements;
+						if (isStringThen(key, context)) {
+							context.report({node: key, messageId: MESSAGE_ID_OBJECT});
+						}
 					}
 				}
 			}
 		},
-		messageId: MESSAGE_ID_OBJECT,
-	},
-	// `export {then}`
-	{
-		selector: 'Identifier',
-		*getNodes(node) {
+
+		// `export {then}`
+		Identifier(node) {
 			if (
 				node.name === 'then' &&
 				node.parent.type === 'ExportSpecifier' &&
 				node.parent.exported === node
 			) {
-				yield node;
+				context.report({node, messageId: MESSAGE_ID_EXPORT});
 			}
-		},
-		messageId: MESSAGE_ID_EXPORT,
-	},
-	// `export function then() {}`,
-	// `export class then {}`,
-	{
-		selector: 'Identifier',
-		*getNodes(node) {
+
+			// `export function then() {}`,
+			// `export class then {}`,
 			if (
 				node.name === 'then' &&
 				(node.parent.type === 'FunctionDeclaration' ||
@@ -152,43 +118,26 @@ const cases = [
 				node.parent.parent.type === 'ExportNamedDeclaration' &&
 				node.parent.parent.declaration === node.parent
 			) {
-				yield node;
+				context.report({node, messageId: MESSAGE_ID_EXPORT});
 			}
 		},
-		messageId: MESSAGE_ID_EXPORT,
-	},
-	// `export const … = …`;
-	{
-		selector: 'VariableDeclaration',
-		*getNodes(node, context) {
-			if (
-				!(
-					node.parent.type === 'ExportNamedDeclaration' &&
-					node.parent.declaration === node
-				)
-			) {
-				return;
-			}
 
-			for (const variable of context.sourceCode.getDeclaredVariables(node)) {
-				if (variable.name === 'then') {
-					yield* variable.identifiers;
+		// `export const … = …`;
+		VariableDeclaration(node) {
+			if (
+				node.parent.type === 'ExportNamedDeclaration' &&
+				node.parent.declaration === node
+			) {
+				for (const variable of context.sourceCode.getDeclaredVariables(node)) {
+					if (variable.name === 'then') {
+						for (const id of variable.identifiers) {
+							context.report({node: id, messageId: MESSAGE_ID_EXPORT});
+						}
+					}
 				}
 			}
 		},
-		messageId: MESSAGE_ID_EXPORT,
-	},
-];
-
-/** @param {import('eslint').Rule.RuleContext} context */
-const create = (context) => {
-	for (const {selector, selectors, messageId, getNodes} of cases) {
-		context.on(selector ?? selectors, function* (node) {
-			for (const problematicNode of getNodes(node, context)) {
-				yield {node: problematicNode, messageId};
-			}
-		});
-	}
+	};
 };
 
 /** @type {import('eslint').Rule.RuleModule} */
